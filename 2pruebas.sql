1dd4a2a0c90fdba01e5151ca226ac208fc956088
JulianManera8 <144916371+JulianManera8@users.noreply.github.com>
2023-12-05 13:15:16 -0300

    Update 2pruebas.sql

--- 2pruebas.sql
+++ 2pruebas.sql
@@ -134,40 +134,43 @@ SELECT AVG(saldo) FROM tblUsuarios WHERE marca = 'LG';
 -- BLOQUE 3
 
 -- 1) Listar el login de los usuarios con nivel 1 o 3
-
+SELECT usuario FROM tblUsuarios WHERE nivel IN('1', '3');
 
 -- 2) Listar nombre y teléfono de los usuarios con teléfono que no sea de la marca BLACKBERRY
-
+SELECT nombre, telefono FROM tblUsuarios WHERE marca != 'BLACKBERRY';
 
 -- 3) Listar el login de los usuarios con nivel 3
-
+SELECT usuario FROM tblUsuarios WHERE nivel = 3;
 
 -- 4) Listar el login de los usuarios con nivel 0
+SELECT usuario FROM tblUsuarios WHERE nivel = 0;
 
 
 -- 5) Listar el login de los usuarios con nivel 1
+SELECT usuario FROM tblUsuarios WHERE nivel = 1;
 
 
 -- 6) Contar el número de usuarios por sexo
+SELECT count(idx), sexo FROM tblUsuarios GROUP BY sexo;
 
 
 -- 7) Listar el login y teléfono de los usuarios con compañia telefónica AT&T
-
+SELECT usuario, telefono FROM tblUsuarios WHERE compañia = 'AT&T';
 
 -- 8) Listar las diferentes compañias en orden alfabético descendentemente
+SELECT DISTINCT compañia FROM tblUsuarios ORDER BY compañia ASC;
 
-
--- 9) Listar el logn de los usuarios inactivos
-
+-- 9) Listar el login de los usuarios inactivos
+SELECT usuario FROM tblUsuarios WHERE activo = '0';
 
 -- 10) Listar los números de teléfono sin saldo
-
+SELECT telefono FROM tblUsuarios WHERE saldo = 0;
 
 -- 11) Calcular el saldo mínimo de los usuarios de sexo “Hombre”
-
+SELECT MIN(saldo) FROM tblUsuarios WHERE sexo = 'H';
 
 -- 12) Listar los números de teléfono con saldo mayor a 300
-
+SELECT telefono, saldo FROM tblUsuarios WHERE saldo > 300;
 
 
 
