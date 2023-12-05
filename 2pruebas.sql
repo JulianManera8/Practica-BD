7d41d55cdc2850af7e6240a211a85b4b6d307d68
JulianManera8 <144916371+JulianManera8@users.noreply.github.com>
2023-12-05 11:53:49 -0300

    Update 2pruebas.sql

--- 2pruebas.sql
+++ 2pruebas.sql
@@ -86,37 +86,47 @@ SELECT nombre, telefono FROM tblUsuarios WHERE marca IN('LG', 'SAMSUNG', 'MOTORO
 -- BLOQUE 2
 
 -- 1) Listar nombre y teléfono de los usuarios con teléfono que no sea de la marca LG o SAMSUNG
+SELECT nombre, telefono FROM tblUsuarios WHERE marca != 'LG' OR marca != 'SAMSUNG';
 
 
 -- 2) Listar el login y teléfono de los usuarios con compañia telefónica IUSACELL
+SELECT usuario, telefono FROM tblUsuarios WHERE compañia = 'IUSACELL';
 
 
 -- 3) Listar el login y teléfono de los usuarios con compañia telefónica que no sea TELCEL
+SELECT usuario, telefono FROM tblUsuarios WHERE compañia != 'TELCEL';
 
 
 -- 4) Calcular el saldo promedio de los usuarios que tienen teléfono marca NOKIA
+SELECT AVG(saldo) FROM tblUsuarios WHERE marca = 'NOKIA';
 
 
 -- 5) Listar el login y teléfono de los usuarios con compañia telefónica IUSACELL o AXEL
+SELECT usuario, telefono FROM tblUsuarios WHERE compañia IN('IUSACELL', 'AXEL');
 
 
 -- 6) Mostrar el email de los usuarios que no usan yahoo
+SELECT email FROM tblUsuarios WHERE email NOT LIKE '%yahoo%';
 
 
 -- 7)Listar el login y teléfono de los usuarios con compañia telefónica que no sea TELCEL o IUSACELL
+SELECT usuario, telefono FROM tblUsuarios WHERE compañia NOT IN('IUSACELL', 'TELCEL');
 
 
 -- 8)Listar el login y teléfono de los usuarios con compañia telefónica UNEFON
+SELECT usuario, telefono FROM tblUsuarios WHERE compañia = 'UNEFON';
 
 
 -- 9)Listar las diferentes marcas de celular en orden alfabético descendentemente
+SELECT DISTINCT marca FROM tblUsuarios ORDER BY marca ASC;
 
 
 -- 10) Listar el login de los usuarios con nivel 0 o 2
+SELECT usuario FROM tblUsuarios WHERE nivel IN('0','2');
 
 
 -- 11) Calcular el saldo promedio de los usuarios que tienen teléfono marca LG
-
+SELECT AVG(saldo) FROM tblUsuarios WHERE marca = 'LG';
 
 
 
